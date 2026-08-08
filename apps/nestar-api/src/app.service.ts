c4663857883787214e3dcb09164e5ffdcff97a68
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-08-08 15:14:16 +0900

    fix: installation packages & modify format satandards

--- apps/nestar-api/src/app.service.ts
+++ apps/nestar-api/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to Nestar APi Server!';
-  }
+	getHello(): string {
+		return 'Welcome to Nestar APi Server!';
+	}
 }
