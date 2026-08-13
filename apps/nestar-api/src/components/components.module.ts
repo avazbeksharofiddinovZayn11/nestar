e200047569b7584bfa91e14150678d18bcf51805
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-08-13 11:42:36 +0900

    feat: create components related modules

--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropetyModule } from './propety/propety.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-  imports: [MemberModule, PropetyModule]
+	imports: [
+		MemberModule,
+		AuthModule,
+		PropetyModule,
+		BoardArticleModule,
+		LikeModule,
+		ViewModule,
+		CommentModule,
+		FollowModule,
+	],
 })
 export class ComponentsModule {}
