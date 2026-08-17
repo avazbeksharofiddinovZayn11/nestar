774e01e52869c104e2003fc3bd24670c55f561a2
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-08-18 04:45:20 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -15,9 +15,9 @@ export class MemberService {
 			const result = await this.memberModel.create(input);
 			// Authentication via TOKEN
 			return result;
-		} catch (err) {
-			console.log('Error, Service.model:', err);
-			throw new BadRequestException(err);
+		} catch (err: any ) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
 		}
 	}
 
