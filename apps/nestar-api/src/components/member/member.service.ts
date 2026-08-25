7f6bca963232d5b6370ed5c30307de45147a3856
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-08-25 18:06:52 +0900

    feat: develop getAllMembersByAdmin and updateMemberByAdmin APIs

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable, InternalServerErrorException } from '@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { Member, Members } from '../../libs/dto/member/member';
-import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentsInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { MemberStatus, MemberType } from '../../libs/enums/member.enum';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
@@ -103,7 +103,7 @@ export class MemberService {
 	}
 
 	public async getAgents(memberId: ObjectId, input: AgentsInquiry): Promise<Members> {
-		const text = input.search;
+		const { text } = input.search;
 		const match: T = { memberType: MemberType.AGENT, memberStatus: MemberStatus.ACTIVE };
 		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
@@ -116,20 +116,44 @@ export class MemberService {
 				{ $sort: sort },
 				{
 					$facet: {
-						list: [{$skip: (input.page -1 ) * input.limit}, {$limit: input.limit}],
-						metaCounter: [{$count: 'total'}],
+						list: [{ $skip: (input.page - 1) * input.limit }, { $limit: input.limit }],
+						metaCounter: [{ $count: 'total' }],
 					},
 				},
 			])
 			.exec();
-		if(!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND)
-		return result[0]
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		return result[0];
 	}
-	public async getAllMembersByAdmin(): Promise<string> {
-		return 'getAllMembersByAdmin executed!';
+	public async getAllMembersByAdmin(input: MembersInquiry): Promise<Members> {
+		const { memberStatus, memberType, text } = input.search;
+		const match: T = {};
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+		if (memberStatus) match.MemberStatus = memberStatus;
+		if (memberType) match.memberType = memberType;
+
+		if (text) match.memberNick = { $regex: new RegExp(text, 'i') };
+		console.log('match:', match);
+
+		const result = await this.memberModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [{ $skip: (input.page - 1) * input.limit }, { $limit: input.limit }],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		return result[0];
 	}
 
-	public async updateMemberByAdmin(): Promise<string> {
-		return 'updateMemberByAdmin executed!';
+	public async updateMemberByAdmin(input: MemberUpdate): Promise<Member> {
+		const result: Member = await this.memberModel.findByIdAndUpdate({ _id: input._id }, input, { new: true }).exec();
+		return result
 	}
 }
--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -1,55 +1,98 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
 import { IsIn, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
-import { MemberAuthType, MemberType } from '../../enums/member.enum';
-import { availableAgentSorts } from '../../config';
+import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
+import { availableAgentSorts, availableMemberSorts } from '../../config';
 import { Direction } from '../../enums/common.enum';
+import { Optional } from '@nestjs/common';
 
 @InputType()
 export class MemberInput {
-  @IsNotEmpty()
-  @Length(3, 12)
-  @Field(() => String)
-  memberNick: string;
+	@IsNotEmpty()
+	@Length(3, 12)
+	@Field(() => String)
+	memberNick: string;
 
-  @IsNotEmpty()
-  @Length(5, 12)
-  @Field(() => String)
-  memberPassword: string;
+	@IsNotEmpty()
+	@Length(5, 12)
+	@Field(() => String)
+	memberPassword: string;
 
-  @IsNotEmpty()
-  @Field(() => String)
-  memberPhone: string;
+	@IsNotEmpty()
+	@Field(() => String)
+	memberPhone: string;
+
+	@IsOptional()
+	@Field(() => MemberType, { nullable: true })
+	memberType?: MemberType;
+
+	@IsOptional()
+	@Field(() => MemberAuthType, { nullable: true })
+	memberAuthType?: MemberAuthType;
+}
+
+@InputType()
+export class LoginInput {
+	@IsNotEmpty()
+	@Length(3, 12)
+	@Field(() => String)
+	memberNick: string;
+
+	@IsNotEmpty()
+	@Length(5, 12)
+	@Field(() => String)
+	memberPassword: string;
+}
+
+@InputType()
+class AISearch {
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	text?: string;
+}
+
+@InputType()
+export class AgentsInquiry {
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	page: number;
+
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	limit: number;
+
+	@IsOptional()
+	@IsIn(availableAgentSorts)
+	@Field(() => String, { nullable: true })
+	sort?: string;
+
+	@IsOptional()
+	@Field(() => Direction, { nullable: true })
+	direction?: Direction;
+
+	@IsNotEmpty()
+	@Field(() => AISearch)
+	search: AISearch;
+}
+
+@InputType()
+class MISearch {
+  @IsOptional()
+  @Field(() => MemberStatus, { nullable: true })
+  memberStatus?: MemberStatus;
 
   @IsOptional()
   @Field(() => MemberType, { nullable: true })
   memberType?: MemberType;
 
   @IsOptional()
-  @Field(() => MemberAuthType, { nullable: true })
-  memberAuthType?: MemberAuthType;
+  @Field(() => String, { nullable: true })
+  text?: string;
 }
 
 @InputType()
-export class LoginInput {
-  @IsNotEmpty()
-  @Length(3, 12)
-  @Field(() => String)
-  memberNick: string;
-
-  @IsNotEmpty()
-  @Length(5, 12)
-  @Field(() => String)
-  memberPassword: string;
-}
-
-@InputType()
-class AISearch{
-  @Field(() => String, {nullable: true})
-  text?: string
-}
-
-@InputType()
-export class AgentsInquiry {
+export class MembersInquiry {
   @IsNotEmpty()
   @Min(1)
   @Field(() => Int)
@@ -61,7 +104,7 @@ export class AgentsInquiry {
   limit: number;
 
   @IsOptional()
-  @IsIn(availableAgentSorts)
+  @IsIn(availableMemberSorts)
   @Field(() => String, { nullable: true })
   sort?: string;
 
@@ -70,6 +113,6 @@ export class AgentsInquiry {
   direction?: Direction;
 
   @IsNotEmpty()
-  @Field(() => AISearch)
-  search: string;
+  @Field(() => MISearch)
+  search: MISearch;
 }
