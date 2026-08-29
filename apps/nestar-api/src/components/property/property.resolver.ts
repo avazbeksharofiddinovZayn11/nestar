bde541127cceb5a89c928c72e6d4d1752d4c5775
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-08-29 16:48:19 +0900

    feat: develop getAllPropertiesByAdmin GraphQl api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,7 +1,12 @@
 import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import {
+	AgentPropertiesInquiry,
+	AllPropertiesInquiry,
+	PropertiesInquiry,
+	PropertyInput,
+} from '../../libs/dto/property/property.input';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { UseGuards } from '@nestjs/common';
@@ -62,19 +67,32 @@ export class PropertyResolver {
 		@Args('input') input: PropertiesInquiry,
 		@AuthMember('_id') memberId: ObjectId,
 	): Promise<Properties> {
-console.log('Query: getProperties')
-return await this.propertyService.getProperties(memberId, input)
+		console.log('Query: getProperties');
+		return await this.propertyService.getProperties(memberId, input);
 	}
 
 	@Roles(MemberType.AGENT)
-@UseGuards(RolesGuard)
-@Query(() => Properties)
-public async getAgentProperties(
-	@Args('input') input: AgentPropertiesInquiry,
-	@AuthMember('_id') memberId: ObjectId,
-): Promise<Properties> {
-	console.log('Query: getAgentProperties');
+	@UseGuards(RolesGuard)
+	@Query(() => Properties)
+	public async getAgentProperties(
+		@Args('input') input: AgentPropertiesInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getAgentProperties');
 
-	return await this.propertyService.getAgentProperties(memberId, input);
-}
+		return await this.propertyService.getAgentProperties(memberId, input);
+	}
+
+	/* ADMIN */
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Query((returns) => Properties)
+	public async getAllPropertiesByAdmin(
+		@Args('input') input: AllPropertiesInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getAllPropertiesByAdmin');
+		return await this.propertyService.getAllPropertiesByAdmin(input);
+	}
 }
