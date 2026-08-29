7e87e50ecd04bec1662c6eab4454eec8fe7fc8f9
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-08-29 17:32:28 +0900

    feat: develop removePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -284,7 +284,7 @@ export class PropertyService {
 
 		return result[0];
 	}
-	
+
 	public async updatePropertyByAdmin(input: PropertyUpdate): Promise<Property> {
 		let { propertyStatus, soldAt, deletedAt } = input;
 
@@ -310,6 +310,21 @@ export class PropertyService {
 				targetKey: 'memberProperties',
 				modifier: -1,
 			});
+		}
+
+		return result;
+	}
+
+	public async removePropertyByAdmin(propertyId: ObjectId): Promise<Property> {
+		const search: T = {
+			_id: propertyId,
+			propertyStatus: PropertyStatus.DELETE,
+		};
+
+		const result = await this.propertyModel.findOneAndDelete(search).exec();
+
+		if (!result) {
+			throw new InternalServerErrorException(Message.REMOVE_FAILED);
 		}
 
 		return result;
