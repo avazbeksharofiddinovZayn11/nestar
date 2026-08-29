7b766f7c40c409af01021dd9659db018125c3e44
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-08-29 16:05:25 +0900

    feat: develo getProperties GraphQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,14 +1,26 @@
 import { ObjectId } from 'bson';
 
-export const availableAgentSorts =['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank']
-export const availableMemberSorts =['createdAt', 'updatedAt', 'memberLikes', 'memberViews']
+export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
+export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
 
+export const availableOptions = ['propertyBarter', 'propertyRent'];
 
- /* IMAGE CONFIGURATION */
+export const availablePropertySorts = [
+	'createdAt',
+	'updatedAt',
+	'propertyLikes',
+	'propertyViews',
+	'propertyRank',
+	'propertyPrice',
+];
+
+/** IMAGE CONFIGURATION **/
+
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+
 export const getSerialForImage = (filename: string) => {
 	const ext = path.parse(filename).ext;
 	return uuidv4() + ext;
@@ -16,4 +28,13 @@ export const getSerialForImage = (filename: string) => {
 
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
+};
+
+export const lookupMember = {
+	$lookup: {
+		from: 'members',
+		localField: 'memberId',
+		foreignField: '_id',
+		as: 'memberData',
+	},
 };
