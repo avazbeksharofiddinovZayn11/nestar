8661135a191a0249bd3832b3061067041dd6ca7e
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-09-01 15:51:47 +0900

    feat: develop boadArticle  related GraphQL APIs

--- apps/nestar-api/src/components/board-article/board-article.module.ts
+++ apps/nestar-api/src/components/board-article/board-article.module.ts
@@ -1,20 +1,25 @@
 import { Module } from '@nestjs/common';
 import { BoardArticleResolver } from './board-article.resolver';
 import { BoardArticleService } from './board-article.service';
-import PropertySchema from '../../schemas/Property.model';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import { MemberModule } from '../member/member.module';
+import BoardArticleSchema from '../../schemas/BoardArticle.model';
 
 @Module({
 	imports: [
-		MongooseModule.forFeature([{ name: 'BoardArticle', schema: PropertySchema }]),
+		MongooseModule.forFeature([
+			{
+				name: 'BoardArticle',
+				schema: BoardArticleSchema,
+			},
+		]),
 		AuthModule,
 		ViewModule,
 		MemberModule,
 	],
 	providers: [BoardArticleResolver, BoardArticleService],
-  exports: [BoardArticleService]
+	exports: [BoardArticleService],
 })
 export class BoardArticleModule {}
