50c9acdcdcc9b7a0052c30789bd6cda4a38b9124
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-09-01 14:34:34 +0900

    feat: configure boardArticle module and inyegrate boardArticle DTO

--- apps/nestar-api/src/components/board-article/board-article.module.ts
+++ apps/nestar-api/src/components/board-article/board-article.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { BoardArticleResolver } from './board-article.resolver';
+import { BoardArticleService } from './board-article.service';
+import PropertySchema from '../../schemas/Property.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
+import { MemberModule } from '../member/member.module';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([{ name: 'BoardArticle', schema: PropertySchema }]),
+		AuthModule,
+		ViewModule,
+		MemberModule,
+	],
+	providers: [BoardArticleResolver, BoardArticleService],
+  exports: [BoardArticleService]
+})
 export class BoardArticleModule {}
--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
-import { PropetyModule } from './propety/propety.module';
+import { PropertyModule } from './property/property.module';
 import { AuthModule } from './auth/auth.module';
 import { CommentModule } from './comment/comment.module';
 import { LikeModule } from './like/like.module';
@@ -8,13 +8,12 @@ import { ViewModule } from './view/view.module';
 import { FollowModule } from './follow/follow.module';
 import { BoardArticleModule } from './board-article/board-article.module';
 import { PropertyResolver } from './property/property.resolver';
-import { PropertyModule } from './property/property.module';
 
 @Module({
 	imports: [
 		MemberModule,
 		AuthModule,
-		PropetyModule,
+		PropertyModule,
 		BoardArticleModule,
 		LikeModule,
 		ViewModule,
