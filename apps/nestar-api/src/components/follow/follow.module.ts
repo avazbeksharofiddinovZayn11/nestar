b62ce63fbd98b228087b402436994d0a7e0ab443
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-09-03 15:24:37 +0900

    feat: create subscribe, unsubscrib, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/components/follow/follow.module.ts
+++ apps/nestar-api/src/components/follow/follow.module.ts
@@ -6,9 +6,11 @@ import FollowSchema from '../../schemas/Follow.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import { LikeModule } from '../like/like.module';
+import { MemberModule } from '../member/member.module';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Follow', schema: FollowSchema }]), AuthModule, ViewModule, LikeModule],
+	imports: [MongooseModule.forFeature([{ name: 'Follow', schema: FollowSchema }]), AuthModule, ViewModule, LikeModule, MemberModule],
 	providers: [FollowResolver, FollowService],
+  exports: [FollowService]
 })
 export class FollowModule {}
